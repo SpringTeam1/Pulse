9c7a9999cc17a88afbda01dbaad4bd662a3a42aa
Sunmin <sunmin323233@gmail.com>
2025-11-06 11:11:29 +0900

    add dbconnection test case

---
+++ src/test/java/com/test/pulse/DBConnectionTests.java
@@ -0,0 +1,14 @@
+package com.test.pulse;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DBConnectionTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
