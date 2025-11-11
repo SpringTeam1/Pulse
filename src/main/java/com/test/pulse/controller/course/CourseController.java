e56ce3731a1becde86f5d557a69f3de9f37a00be
Sunmin <sunmin323233@gmail.com>
2025-11-12 01:05:38 +0900

    manualcourseregister 파일생성 및 controller-view 연결

--- src/main/java/com/test/pulse/controller/course/CourseController.java
+++ src/main/java/com/test/pulse/controller/course/CourseController.java
@@ -7,10 +7,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/course")
 public class CourseController {
-	@GetMapping("/register")
-	public String gpxRegPage() {
-		return "course.gpxcourseregister";
+	
+	/**
+	 * 코스 메인페이지
+	 * @return
+	 */
+	@GetMapping("/main")
+	public String courseMain() {
+		return "course.coursemain";
 	}
+	
+	/**
+	 * 사용자가 gpx를 첨부하여 코스 등록 요청
+	 * @return
+	 */
+	@GetMapping("/gpxregister")
+	public String gpxRegPage() {
+		return "script.course.gpxcourseregister";
+	}
+	/**
+	 * 사용자가 직접 지도 좌표를 찍어서 코스 등록 요청
+	 * @return
+	 */
+	@GetMapping("/manualregister")
+	public String manualRegPage() {
+		return "script.course.manualcourseregister";
+	}
+	
 	
 	
 }
