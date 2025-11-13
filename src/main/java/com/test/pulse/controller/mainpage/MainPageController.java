77fc7f8a8f374dc629c2737ece1fa4af51b6a65c
hh-gun <ghdrjs2314@naver.com>
2025-11-13 09:39:30 +0900

    메인페이지 부분 삭제

--- src/main/java/com/test/pulse/controller/mainpage/MainPageController.java
+++ src/main/java/com/test/pulse/controller/mainpage/MainPageController.java
@@ -1,13 +1,5 @@
 package com.test.pulse.controller.mainpage;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
 public class MainPageController {
 
-    @GetMapping("/index.do")
-    public String mainPage() {
-        return "main.index"; // WEB-INF/views/main/index.jsp
-    }
 }
