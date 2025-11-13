5b4faea59878877a31cbafd676eb6813e16e4249
hh-gun <ghdrjs2314@naver.com>
2025-11-13 09:45:40 +0900

    공지게시판 파일 생성

--- src/main/java/com/test/pulse/controller/boardnotice/BoardNoticeController.java
+++ src/main/java/com/test/pulse/controller/boardnotice/BoardNoticeController.java
@@ -1,6 +1,23 @@
 package com.test.pulse.controller.boardnotice;
 
-public class BoardNoticeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/boardnotice")
+public class BoardNoticeController {
 	
+	@GetMapping("/list.do")
+    public String boardNoticeList() {
+        
+		return "boardnotice.list";
+    }
+	
+	@GetMapping("/add.do")
+    public String boardNoticeAdd() {
+        
+		return "boardnotice.add";
+    }
+
 }
---
+++ src/main/java/com/test/pulse/service/boardnotice/BoardNoticeService.java
@@ -0,0 +1,5 @@
+package com.test.pulse.service.boardnotice;
+
+public class BoardNoticeService {
+
+}
