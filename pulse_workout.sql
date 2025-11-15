40f6a32ce60c1a0fc69c62248083fd85287faa81
Sunmin <sunmin323233@gmail.com>
2025-11-16 02:27:35 +0900

    운동기록 파일세팅 2

---
+++ pulse_workout.sql
@@ -0,0 +1,23 @@
+DROP SEQUENCE seqWorkout;
+DROP TABLE tblWorkout;
+
+CREATE SEQUENCE seqWorkout;
+CREATE TABLE tblWorkout (
+	exerciseRecordSeq	NUMBER			NOT NULL, -- 운동 기록 번호(PK)
+	accountId			VARCHAR2(100)	NOT NULL, -- 아이디(FK)
+	trackData			CLOB			NOT NULL, -- JSON 파일(.gpx)
+	totalTime			NUMBER			NOT NULL, -- 운동 시간
+	totalDistance		NUMBER			NOT NULL, -- 운동 거리
+	totalCalories		NUMBER			NOT NULL, -- 소모 칼로리
+	workoutDate			DATE			NOT NULL, -- 운동 날짜
+	avgPace				NUMBER			NOT NULL, -- 평균 페이스
+	attachment			VARCHAR2(200)	NULL,     -- 첨부파일(사진 등)
+	exerciseComment		VARCHAR2(500)	NULL      -- 사용자 코멘트(운동 기록에 대한)
+);
+
+ALTER TABLE tblWorkout ADD CONSTRAINT "PK_TBLWORKOUT" PRIMARY KEY (exerciseRecordSeq);
+
+ALTER TABLE tblWorkout ADD CONSTRAINT "FK_tblAccountInfo_TO_tblWorkout_1"
+FOREIGN KEY (accountId)
+REFERENCES tblAccountInfo (accountId);
+select * from tblWorkout;
--- src/main/java/com/test/pulse/controller/workout/WorkoutController.java
+++ src/main/java/com/test/pulse/controller/workout/WorkoutController.java
@@ -1,4 +1,4 @@
-package com.test.pulse.controller;
+package com.test.pulse.controller.workout;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
